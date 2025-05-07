b323f60aff00c2facb1aa6a71f7ecfb3063064c9
P.B. <patrykbochenski3@gmail.com>
2025-05-07 21:36:22 +0200

    feat: Add gitignore/css file

--- app/pages/layout.tsx
+++ app/pages/layout.tsx
@@ -1,15 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "../styles/appstyle.css";
 
 import { Outlet } from 'react-router';
 
 export default function Layout() {
     return (
         <>
-            <h1 className="text-center">Header</h1>
+            <header>Header</header>
             <Outlet></Outlet>
-            <footer id="Footer">
-                <h1 className="text-center">Footer</h1>
-            </footer>
+            <footer>Footer</footer>
         </>
     );
 }
